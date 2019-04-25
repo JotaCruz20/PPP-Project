0d4bee8f125111ea86d56077e069c4b062fbd535
Joao Cruz <joao.scruz2000@gmail.com>
2019-04-25 22:17:26 +0100

    Check se login ja existe ou nao

--- main.c
+++ main.c
@@ -2,47 +2,13 @@
 #include <string.h>
 #include <errno.h>
 
-void registo(FILE *file){
-    int len;
-    char name[50],cid[20],data[12],tele[9];
-    if(file==NULL){
-        printf("Erro %s", strerror(errno));
-    }
-    printf("Indique: username-\n");//Nome
-    fgets(name,50,stdin);
-    len=strlen(name);
-    name[len-1]='\0';//Tirar o paragrafo
-    fputs(name,file);//Inserir no ficheiro
-    fputs(" ",file);//Adicionar espaço
-    printf("Indique: cidade-\n");//Cidade
-    fgets(cid,20,stdin);
-    len=strlen(cid);
-    cid[len-1]='\0';
-    fputs(cid,file);
-    fputs(" ",file);
-    printf("Indique: data de nascimento-\n");//Data nascimento
-    fgets(data,12,stdin);
-    len=strlen(data);
-    data[len-1]='\0';
-    fputs(data,file);
-    fputs(" ",file);
-    printf("Indique: nº telemovel-\n");//Nº telemovel
-    fgets(tele,9,stdin);
-    fputs(tele,file);
-    fputs("\n",file);
-}
-int login(FILE *file){
-    int i,count=0,len;
-    char read[100],user[50],string[50];
+int logtester(FILE *file, char name[50]){//serve para ver se o nome dado existe no file
+    char read[100],string[50];
     char *ptr;
-    printf("\nIndique o seu username: ");
-    fgets(user,50,stdin);
-    len=strlen(user);
-    user[len-1]='\0';//tirar o \n da string para se poder comparar
+    int i,count=0;
     if(file==NULL){
         printf("Erro %s", strerror(errno));
     }
-    fseek(file, 0, SEEK_SET );
     while(fgets(read,100,file)!=NULL){
         ptr=read;
         for(i=0;*(ptr+i)!='\0';i++){
@@ -52,8 +18,7 @@ int login(FILE *file){
             }
             else{
                 string[count]='\0';
-                if(strcmp(string,user)==0){
-                    printf("1");
+                if(strcmp(string,name)==0){
                     return 1;
                 }
                 count=0;//para começar string do 0
@@ -62,6 +27,58 @@ int login(FILE *file){
     }
     return 0;
 }
+
+void registo(FILE *file1, FILE *file2){//funcao para o registo
+    int len,log;
+    char name[50],cid[20],data[12],tele[9];
+    if(file1==NULL){
+        printf("Erro %s", strerror(errno));
+    }
+    do {
+        printf("Indique: username-\n");//Nome
+        fgets(name, 50, stdin);
+        len = strlen(name);
+        name[len - 1] = '\0';//Tirar o paragrafo
+        log=logtester(file2,name);//Para confirmar que nao existe um user com o mesmo username
+        if(log!=0){
+            printf("\nO username ja esta em uso, use outro.\n");
+        }
+    }while(log!=0);
+    fputs(name, file1);//Inserir no ficheiro
+    fputs(" ", file1);//Adicionar espaço
+    printf("Indique: cidade-\n");//Cidade
+    fgets(cid, 20, stdin);
+    len = strlen(cid);
+    cid[len - 1] = '\0';
+    fputs(cid, file1);
+    fputs(" ", file1);
+    printf("Indique: data de nascimento-\n");//Data nascimento
+    fgets(data, 12, stdin);
+    len = strlen(data);
+    data[len - 1] = '\0';
+    fputs(data, file1);
+    fputs(" ", file1);
+    printf("Indique: nº telemovel-\n");//Nº telemovel
+    fgets(tele, 9, stdin);
+    fputs(tele, file1);
+    fputs("\n", file1);
+}
+int login(FILE *file){//funcao para o login
+    int len,logf;
+    char user[50];
+    printf("\nIndique o seu username: ");
+    fgets(user,50,stdin);
+    len=strlen(user);
+    user[len-1]='\0';//tirar o \n da string para se poder comparar
+    logf=logtester(file,user);
+    if(logf==1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
 
 int main() {
     int n,log;
@@ -77,17 +94,19 @@ int main() {
         log=login(frr);
     }
     else{
-        registo(fra);
+        registo(fra,frr);
     }
     while(log==0){
         printf("\nO seu user nao existe, pretende: 1-Criar um / 2-Tentar outra vez: ");
         scanf("%d", &n);
         getchar();
         if(n==1){
-            registo(fra);
+            registo(fra,frr);
         }
         else{
             log=login(frr);
         }
     }
+    printf("MENU");
+    printf("\nEscolha o que quer fazer:\n1-Alterar dados\n2-Listagem dos Locais e PDIs\n3-Preferencias\n4-Construir Viagem.");
 }
